9d82fb86340a3d523f00b4679edab62bb86e6c61
Andrewslayton <andrewsla1726@gmail.com>
2024-04-09 01:36:41 -0400

    buttons

---
+++ src/app/components/expand/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const ConnectedButton = ({
+  label,
+  additionalClasses,
+}: {
+  label: string,
+  additionalClasses?: string,
+}) => (
+  <button
+    className={`p-4 bg-blue-500 text-white rounded-full focus:outline-none focus:ring-4 focus:ring-green-300 transition-all duration-300 ease-out transform ${additionalClasses}`}
+  >
+    {label}
+  </button>
+);
+export default ConnectedButton;
