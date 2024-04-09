fe8beb3b7aac73cc14d9883b541de1275a2ac4e4
Andrewslayton <andrewsla1726@gmail.com>
2024-04-09 04:09:49 -0400

    doom

--- src/app/components/experience/experiences.tsx
+++ src/app/components/experience/experiences.tsx
@@ -1,3 +1,75 @@
-export default function Experience() {
-  return <h1>About Me</h1>;
+export function Experience() {
+  return (
+    <div className="bg-gray-500 p-5 rounded-lg overflow-auto h-full">
+      <h1 className="text-xl font-bold mb-4">About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia
+        odio vitae vestibulum vestibulum. Sed nec felis pellentesque, lacinia
+        dui sed, ultricies sapien. Pellentesque orci lectus, consectetur vel,
+        posuere sit amet dui. Ut in egestas lectus. Etiam tempor odio tellus, at
+        bibendum neque faucibus quis. Sed vel facilisis elit. Nullam fringilla
+        pharetra diam non accumsan. Morbi eget aliquam mauris. Etiam vehicula
+        efficitur mi.
+      </p>
+      <p className="mt-4">
+        Aenean nec tempus velit. Praesent gravida mi et mauris sollicitudin
+        ultricies. Duis mollis, est non commodo luctus, nisi erat porttitor
+        ligula, eget lacinia odio sem nec elit. Cras vitae condimentum felis.
+        Nulla at venenatis eros. Sed condimentum turpis arcu, ut cursus quam
+        semper vitae. In scelerisque libero ut elit porttitor commodo
+        Suspendisse laoreet magna.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia
+        odio vitae vestibulum vestibulum. Sed nec felis pellentesque, lacinia
+        dui sed, ultricies sapien. Pellentesque orci lectus, consectetur vel,
+        posuere sit amet dui. Ut in egestas lectus. Etiam tempor odio tellus, at
+        bibendum neque faucibus quis. Sed vel facilisis elit. Nullam fringilla
+        pharetra diam non accumsan. Morbi eget aliquam mauris. Etiam vehicula
+        efficitur mi.
+      </p>
+      <p className="mt-4">
+        Aenean nec tempus velit. Praesent gravida mi et mauris sollicitudin
+        ultricies. Duis mollis, est non commodo luctus, nisi erat porttitor
+        ligula, eget lacinia odio sem nec elit. Cras vitae condimentum felis.
+        Nulla at venenatis eros. Sed condimentum turpis arcu, ut cursus quam
+        semper vitae. In scelerisque libero ut elit porttitor commodo
+        Suspendisse laoreet magna.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia
+        odio vitae vestibulum vestibulum. Sed nec felis pellentesque, lacinia
+        dui sed, ultricies sapien. Pellentesque orci lectus, consectetur vel,
+        posuere sit amet dui. Ut in egestas lectus. Etiam tempor odio tellus, at
+        bibendum neque faucibus quis. Sed vel facilisis elit. Nullam fringilla
+        pharetra diam non accumsan. Morbi eget aliquam mauris. Etiam vehicula
+        efficitur mi.
+      </p>
+      <p className="mt-4">
+        Aenean nec tempus velit. Praesent gravida mi et mauris sollicitudin
+        ultricies. Duis mollis, est non commodo luctus, nisi erat porttitor
+        ligula, eget lacinia odio sem nec elit. Cras vitae condimentum felis.
+        Nulla at venenatis eros. Sed condimentum turpis arcu, ut cursus quam
+        semper vitae. In scelerisque libero ut elit porttitor commodo
+        Suspendisse laoreet magna.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia
+        odio vitae vestibulum vestibulum. Sed nec felis pellentesque, lacinia
+        dui sed, ultricies sapien. Pellentesque orci lectus, consectetur vel,
+        posuere sit amet dui. Ut in egestas lectus. Etiam tempor odio tellus, at
+        bibendum neque faucibus quis. Sed vel facilisis elit. Nullam fringilla
+        pharetra diam non accumsan. Morbi eget aliquam mauris. Etiam vehicula
+        efficitur mi.
+      </p>
+      <p className="mt-4">
+        Aenean nec tempus velit. Praesent gravida mi et mauris sollicitudin
+        ultricies. Duis mollis, est non commodo luctus, nisi erat porttitor
+        ligula, eget lacinia odio sem nec elit. Cras vitae condimentum felis.
+        Nulla at venenatis eros. Sed condimentum turpis arcu, ut cursus quam
+        semper vitae. In scelerisque libero ut elit porttitor commodo
+        Suspendisse laoreet magna.
+      </p>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,90 +1,44 @@
 "use client";
-import { ConnectedButton } from "./components/expand/connectButton";
-import { HyperspaceBackground } from "./components/background/animatedback";
 import React, { useState } from "react";
-import Experience from "./components/experience/experiences";
-import Projects from "./components/projects/projects";
-import Education from "./components/education/education";
-import Aboutme from "./components/about/aboutme";
-
+import {HyperspaceBackground} from "./components/background/animatedback";
+import {Aboutme} from "./components/about/aboutme";
+import {Education} from "./components/education/education";
+import {Experience} from "./components/experience/experiences";
+import {Projects} from "./components/projects/projects";
+import { ConnectedButton } from "./components/expand/connectButton";
 
 const HomePage = () => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [currentView, setCurrentView] = useState("home");
-  
-const renderContent = () => {
-  switch (currentView) {
-    case "experience":
-      return <Experience />;
-    case "projects":
-      return <Projects />;
-    case "education":
-      return <Education />;
-    case "about":
-      return <Aboutme />;
-    default:
-      return null; 
-  }
-};
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
-  };
 
   return (
     <div className="flex items-center justify-center h-screen bg-black relative">
       <HyperspaceBackground isVisible={isExpanded} />
 
-
-
-
-      <div className="z-10 flex flex-wrap items-center justify-center space-x-4 space-y-20">
-        <div
-          className={`flex-1 flex justify-between ${
-            isExpanded ? "visible" : "invisible"
-          }`}
+      {!isExpanded && (
+        <button
+          onClick={() => setIsExpanded(true)}
+          className="z-10 absolute p-6 bg-blue-500 text-white rounded-full focus:outline-none focus:ring-4 focus:ring-blue-300 transition-all duration-300 ease-out"
         >
-          <ConnectedButton
-            label="Experience"
-            additionalClasses={
-              isExpanded ? "opacity-100 scale-100" : "opacity-0 scale-0"
-            }
-          />
-          <ConnectedButton
-            label="Projects"
-            additionalClasses={
-              isExpanded ? "opacity-100 scale-100" : "opacity-0 scale-0"
-            }
-          />
-        </div>
+          Dive In
+        </button>
+      )}
 
-        <div className="flex justify-center w-full">
-          <button
-            onClick={toggleExpand}
-            className="p-6 bg-blue-500 text-white rounded-full focus:outline-none focus:ring-4 focus:ring-blue-300 transition-all duration-300 ease-out"
-          >
-            Dive In
-          </button>
+      {isExpanded && (
+        <div className="grid grid-cols-2 grid-rows-2 gap-6 w-full h-full absolute">
+          <div className="overflow-auto p-10 bg-opacity-90 bg-black text-white rounded-lg">
+            <Aboutme />
+          </div>
+          <div className="overflow-auto p-10 bg-opacity-90 bg-black text-white rounded-lg">
+            <Experience />
+          </div>
+          <div className="overflow-auto p-10 bg-opacity-90 bg-black text-white rounded-lg">
+            <Education />
+          </div>
+          <div className="overflow-auto p-10 bg-opacity-90 bg-black text-white rounded-lg">
+            <Projects />
+          </div>
         </div>
-
-        <div
-          className={`flex-1 flex justify-between ${
-            isExpanded ? "visible" : "invisible"
-          }`}
-        >
-          <ConnectedButton
-            label="Education"
-            additionalClasses={
-              isExpanded ? "opacity-100 scale-100" : "opacity-0 scale-0"
-            }
-          />
-          <ConnectedButton
-            label="About Me"
-            additionalClasses={
-              isExpanded ? "opacity-100 scale-100" : "opacity-0 scale-0"
-            }
-          />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
